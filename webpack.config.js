22f220b12b7901354912d917fb061c6fc30ae9ec
yuya.sato <yuya.sato@livesense.co.jp>
2022-06-27 13:20:59 +0900

    TKのeslint, prettierルールを導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  entry: "./src/index.tsx",
+  mode: 'development',
+  entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "main.js",
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
   },
   module: {
     rules: [
@@ -14,31 +13,31 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         use: [
           {
-            loader: "babel-loader",
-            options: { presets: ["@babel/preset-env", "@babel/react"] },
+            loader: 'babel-loader',
+            options: { presets: ['@babel/preset-env', '@babel/react'] },
           },
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
-              configFile: path.resolve(__dirname, "tsconfig.json"),
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
             },
           },
         ],
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, 'dist'),
     },
     port: 3000,
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
-  target: "web",
+  target: 'web',
 };
